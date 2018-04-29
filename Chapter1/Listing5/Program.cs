80a1969ad6e4164e364959213514765766c5aad6
Lei Zhao <LeiZh26@gmail.com>
2018-04-30 04:44:35 +0800

    update .gitignore and add Listing 5

---
+++ Chapter1/Listing5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Listing5
+{
+    public static class Program
+    {
+        [ThreadStatic]
+        public static int _field = 0;
+        public static void Main(string[] args)
+        {
+            new Thread(() =>
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            _field++;
+                            Console.WriteLine("Thread A: {0}", _field);
+                        }
+                        
+                    }).Start();
+            new Thread(() =>
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            _field++;
+                            Console.WriteLine("Thread B: {0}", _field);
+                        }
+                    }).Start();
+            // Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            // stopped = true;
+            // t.Join();
+        }
+    }
+}
